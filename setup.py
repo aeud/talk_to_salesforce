3f3b0dc09aeb189f5de26d2c7a5a84794e0ec4f5
Adrien Eudes <adrien.eudes@gmail.com>
2024-04-25 10:26:13 +0200

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="talk_to_salesforce",
-    version="2.0.1",
+    version="2.0.2",
     packages=find_packages(),
     install_requires=[
         "click",
